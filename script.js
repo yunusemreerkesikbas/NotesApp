84c6dcdf38cb8c9e4e1ad317a717f1633eb0e7f4
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2022-07-06 02:39:55 +0300

    dynamic add  note button added

--- script.js
+++ script.js
@@ -1,25 +1,70 @@
-const notesEL = document.querySelector(".notes");
-const editBtn = document.querySelector(".edit");
-const deleteBtn = document.querySelector(".delete");
+const addBtn = document.getElementById('add');
+// const notesEL = document.querySelector(".notes");
+const notes = JSON.parse(localStorage.getItem("notes"));
+if (notes) {
+    
+    notes.forEach(note => {
+        addNewNote(note);
+    });
+}
 
-const main = notesEL.querySelector(".main");
-const textArea = notesEL.querySelector("textarea");
+addBtn.addEventListener('click', () => {
 
-editBtn.addEventListener("click", () => {
-    main.classList.toggle("hidden");
-    textArea.classList.toggle("hidden");
-})
+    addNewNote();
+    // yeni nota bağlandık
+});
 
-textArea.addEventListener("input", (e) => {
-    const { value } = e.target;
-    main.innerHTML = marked(value);
-}) 
+function addNewNote(text = "") {
 
-// <!-- <div class="notes">
-// <div class="tools">
-//     <button class="edit"><i class="fas fa-edit"></i></button>
-//     <button class="delete"><i class="fas fa-trash-alt"></i></button>
-// </div>
-// <div class="main hidden"> </div>
-// <textarea></textarea>
-// </div> -->
+    const note = document.createElement('div');
+    note.classList.add('note');
+    // butona basınca oluşacak yapı
+    note.innerHTML = `
+        <div class="notes">
+            <div class="tools">
+                <button class="edit"><i class="fas fa-edit"></i></button>
+                <button class="delete"><i class="fas fa-trash-alt"></i></button>
+            </div>
+            <div class="main ${ text ? "" : "hidden"}"> </div>
+            <textarea class="main ${ text ? "hidden" : ""}"></textarea>
+        </div>`;
+
+
+        // edit ve delete butonlarını aldık
+    const editBtn = note.querySelector(".edit");
+    const deleteBtn = note.querySelector(".delete");
+
+    const main = note.querySelector(".main");
+    const textArea = note.querySelector("textarea");
+    textArea.value = text;
+    main.innerHTML = marked(text);
+    editBtn.addEventListener("click", () => {
+        main.classList.toggle("hidden");
+        textArea.classList.toggle("hidden");
+    })
+    deleteBtn.addEventListener("click", () => {
+        note.remove();
+
+        updateLS();
+
+    });
+    textArea.addEventListener("input", (e) => {
+        const { value } = e.target;
+        main.innerHTML = marked(value);
+        updateLS()
+
+    }) 
+//test
+
+    document.body.appendChild(note);
+}
+function updateLS(){
+
+    // local storage ekledik
+    const notesText = document.querySelectorAll('textarea');
+    const notes = [];
+    notesText.forEach((note) => {
+        notes.push(note.value);
+    });
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
